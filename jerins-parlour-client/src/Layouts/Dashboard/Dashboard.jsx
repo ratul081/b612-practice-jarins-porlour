d06d923e5fa55e06df1a30d99eaf85fcc7841e08
ratul081 <ratullinux77@gmail.com>
2023-11-10 16:48:45 +0600

    updating desh pc

--- jerins-parlour-client/src/Layouts/Dashboard/Dashboard.jsx
+++ jerins-parlour-client/src/Layouts/Dashboard/Dashboard.jsx
@@ -48,7 +48,7 @@ const Dashboard = () => {
                     </svg>
                     <span className="mx-4 font-medium">Dashboard</span>
                   </a>
-                  <a
+                  <a 
                     className="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
                     href="#">
                     <svg
